4e1f1626a770c3fe35db17dd9a8a2a83be8a533c
aysunsevnc <97788573+aysunsevnc@users.noreply.github.com>
2022-02-14 00:14:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            byte b = 5;
+            sbyte c =5;
+            ushort us =5
+            Int16 i16 =2;
+            Int i =2;
+            Int32 i32 =2;
+            int64 i46 =2;
+            uInt ui=2;
+            long l =4;
+            ulong ul =4;
+            float f =5;
+            double d =5;
+            decimal de = 5;
+            char c ='2'; //2 bye
+            string str ='2'//sınırsız
+            bool b1 =true;
+            bool b2 =false ;
+            DateTime dt =DateTime.Now;
+            Console.WriteLine(dt)
+
+        }
+    }
+}
